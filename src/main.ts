487e4b153263d3188cda94e8dfac80c51c17a9b3
Joao Lourenco <joaodslourenco@gmail.com>
2023-07-06 10:31:13 -0300

    feat: add errors and interceptors

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { UnauthorizedInterceptor } from './common/errors/interceptors/unauthorized.interceptor';
+import { NotFoundInterceptor } from './common/errors/interceptors/notfound.interceptor';
+import { DatabaseInterceptor } from './common/errors/interceptors/database.interceptor';
+import { ConflictInterceptor } from './common/errors/interceptors/conflict.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +16,14 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
+  app.useGlobalInterceptors(
+    new UnauthorizedInterceptor(),
+    new NotFoundInterceptor(),
+    new DatabaseInterceptor(),
+    new ConflictInterceptor(),
+  );
+
   await app.listen(3000);
 }
 bootstrap();
