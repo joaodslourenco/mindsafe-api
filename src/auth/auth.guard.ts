d41c613b0feea7ec401d9341fc7533e298cfbb50
Joao Lourenco <joaodslourenco@gmail.com>
2023-07-10 15:25:52 -0300

    feat: improve error msgs at auth guard

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -1,14 +1,10 @@
-import {
-  CanActivate,
-  ExecutionContext,
-  Injectable,
-  UnauthorizedException,
-} from '@nestjs/common';
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { jwtConstants } from './constants';
 import { Request } from 'express';
 import { IS_PUBLIC_KEY } from 'src/common/decorators/public';
 import { Reflector } from '@nestjs/core';
+import { UnauthorizedError } from 'src/common/errors/types/UnauthorizedError';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
@@ -26,17 +22,18 @@ export class AuthGuard implements CanActivate {
     const request = context.switchToHttp().getRequest();
     const token = this.extractTokenFromHeader(request);
     if (!token) {
-      throw new UnauthorizedException();
+      throw new UnauthorizedError('No token found.');
     }
     try {
       const payload = await this.jwtService.verifyAsync(token, {
         secret: jwtConstants.secret,
       });
-      // 💡 We're assigning the payload to the request object here
-      // so that we can access it in our route handlers
+
       request['user'] = payload;
     } catch {
-      throw new UnauthorizedException();
+      throw new UnauthorizedError(
+        'There was an error while verifying the token',
+      );
     }
     return true;
   }
