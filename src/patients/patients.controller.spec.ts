2f834b4bd319ecc2817cf5c0c9b98075a910dc33
Joao Lourenco <joaodslourenco@gmail.com>
2023-07-12 16:33:26 -0300

    test: better create method at patient controller

--- src/patients/patients.controller.spec.ts
+++ src/patients/patients.controller.spec.ts
@@ -2,8 +2,14 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { PatientsController } from './patients.controller';
 import { PatientsService } from './patients.service';
 import { PatientEntity } from './entities/patient.entity';
-import { PrismaService } from 'src/prisma/prisma.service';
-import { PatientsRepository } from './repositories/patients.repository';
+
+const testPatient: PatientEntity = {
+  id: '13216546',
+  name: 'test',
+  email: 'teste@teste.com',
+  password: '123456',
+  createdAt: new Date(),
+};
 
 describe('PatientsController', () => {
   let patientsController: PatientsController;
@@ -12,7 +18,19 @@ describe('PatientsController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [PatientsController],
-      providers: [PatientsService, PatientsRepository, PrismaService],
+      providers: [
+        {
+          provide: PatientsService,
+          useValue: {
+            create: jest.fn().mockResolvedValue(testPatient),
+            findOne: jest.fn().mockResolvedValue(testPatient),
+            findAll: jest.fn(),
+            findAllPostsByPatient: jest.fn(),
+            update: jest.fn(),
+            remove: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     patientsController = module.get<PatientsController>(PatientsController);
@@ -24,30 +42,27 @@ describe('PatientsController', () => {
   });
 
   describe('create method (POST)', () => {
-    const testPatient: PatientEntity = {
-      id: '13216546',
-      name: 'test',
-      email: 'teste@teste.com',
-      password: '123456',
-      createdAt: new Date(),
-    };
-
-    beforeEach(() => {
-      jest
-        .spyOn(patientsService, 'create')
-        .mockImplementation(async () => testPatient);
-    });
-
-    it('user should return as defined', async () => {
+    it('should return as defined', async () => {
       const newUser = await patientsController.create(testPatient);
 
       expect(newUser).toBeDefined();
+    });
+
+    it('should have called patientsService Create', async () => {
+      patientsController.create(testPatient);
+      expect(patientsService.create).toHaveBeenCalled();
     });
 
     it('should return the new patient obj with the same keys', async () => {
       const newUser = await patientsController.create(testPatient);
 
       expect(Object.keys(newUser)).toEqual(Object.keys(testPatient));
+    });
+
+    it("should throw an exception when there's an error", () => {
+      jest.spyOn(patientsService, 'create').mockRejectedValueOnce(new Error());
+
+      expect(patientsController.create(testPatient)).rejects.toThrowError();
     });
   });
 });
