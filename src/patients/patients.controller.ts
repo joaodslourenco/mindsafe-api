e6479ab1c53ef6ad642702b1a330127f0454e6d7
Joao Lourenco <joaodslourenco@gmail.com>
2023-07-13 10:03:29 -0300

    fix: create patient route should be public

--- src/patients/patients.controller.ts
+++ src/patients/patients.controller.ts
@@ -11,12 +11,14 @@ import { PatientsService } from './patients.service';
 import { CreatePatientDto } from './dto/create-patient.dto';
 import { UpdatePatientDto } from './dto/update-patient.dto';
 import { ApiTags } from '@nestjs/swagger';
+import { Public } from 'src/common/decorators/public';
 
 @ApiTags('Patients')
 @Controller('patients')
 export class PatientsController {
   constructor(private readonly patientsService: PatientsService) {}
 
+  @Public()
   @Post()
   create(@Body() createPatientDto: CreatePatientDto) {
     return this.patientsService.create(createPatientDto);
