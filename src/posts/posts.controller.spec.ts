e10e2e51f63ad1b7a04b175b8ece5f4bdca4f2d8
Joao <joaodslourenco@gmail.com>
2023-07-19 14:43:41 -0300

    test: post create controller

--- src/posts/posts.controller.spec.ts
+++ src/posts/posts.controller.spec.ts
@@ -1,20 +1,71 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PostsController } from './posts.controller';
 import { PostsService } from './posts.service';
+import { PostEntity } from './entities/post.entity';
+import { CreatePostDto } from './dto/create-post.dto';
+
+const examplePost: PostEntity = {
+  id: 'some-id',
+  content: 'some content',
+  patientId: 'patientId',
+  createdAt: new Date(),
+};
 
 describe('PostsController', () => {
-  let controller: PostsController;
+  let postsController: PostsController;
+  let postsService: PostsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [PostsController],
-      providers: [PostsService],
+      providers: [
+        {
+          provide: PostsService,
+          useValue: {
+            create: jest.fn().mockResolvedValue(examplePost),
+            findAll: jest.fn(),
+            findOne: jest.fn(),
+            update: jest.fn(),
+            remove: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
-    controller = module.get<PostsController>(PostsController);
+    postsController = module.get<PostsController>(PostsController);
+    postsService = module.get<PostsService>(PostsService);
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(postsController).toBeDefined();
+  });
+
+  describe('create method (POST)', () => {
+    const newPost: CreatePostDto = {
+      content: 'new post',
+      patientId: 'some-patient-id',
+    };
+    it('should return as defined', async () => {
+      const newUser = await postsController.create(newPost);
+
+      expect(newUser).toBeDefined();
+    });
+
+    it('should have called postsService Create', async () => {
+      postsController.create(newPost);
+      expect(postsService.create).toHaveBeenCalled();
+    });
+
+    it('should return the new patient obj with the same keys', async () => {
+      const newUser = await postsController.create(newPost);
+
+      expect(Object.keys(newUser)).toEqual(Object.keys(examplePost));
+    });
+
+    it("should throw an exception when there's an error", () => {
+      jest.spyOn(postsService, 'create').mockRejectedValueOnce(new Error());
+
+      expect(postsController.create(newPost)).rejects.toThrowError();
+    });
   });
 });
