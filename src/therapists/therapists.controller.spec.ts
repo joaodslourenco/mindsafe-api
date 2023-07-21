313313d9a0f73338da2ced22c94e73b0ad726714
Joao <joaodslourenco@gmail.com>
2023-07-21 11:18:25 -0300

    test: create therapist controller test

--- src/therapists/therapists.controller.spec.ts
+++ src/therapists/therapists.controller.spec.ts
@@ -1,20 +1,46 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TherapistsController } from './therapists.controller';
 import { TherapistsService } from './therapists.service';
+import { UpdatePatientDto } from 'src/patients/dto/update-patient.dto';
+
+const testTherapist = {};
+
+const arrayOfTherapists = [testTherapist, testTherapist, testTherapist];
 
 describe('TherapistsController', () => {
-  let controller: TherapistsController;
+  let therapistsController: TherapistsController;
+  let therapistsService: TherapistsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [TherapistsController],
-      providers: [TherapistsService],
+      providers: [
+        {
+          provide: TherapistsService,
+          useValue: {
+            create: jest.fn().mockResolvedValue(testTherapist),
+            findOne: jest.fn().mockResolvedValue(testTherapist),
+            findAll: jest.fn().mockResolvedValue(arrayOfTherapists),
+            update: jest
+              .fn()
+              .mockImplementation(
+                (id: string, updateDto: UpdatePatientDto) => ({
+                  id,
+                  ...updateDto,
+                }),
+              ),
+            remove: jest.fn().mockResolvedValue(testTherapist),
+          },
+        },
+      ],
     }).compile();
 
-    controller = module.get<TherapistsController>(TherapistsController);
+    therapistsController =
+      module.get<TherapistsController>(TherapistsController);
+    therapistsService = module.get<TherapistsService>(TherapistsService);
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(therapistsController).toBeDefined();
   });
 });
